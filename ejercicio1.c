4e1fee4c11a616037d4914576a4da238996f0b38
katukis <katukis@outlook.com>
2025-11-07 20:38:11 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d", &a);
     printf("Valor 2: ");
     scanf("%d", &b);
-    while (a <= 0 || b <= 0 || a < b);
+    while (a <= 0 || b <= 0 || a < b)
     {
         printf("\nLos valores deben ser positivos y el primero mayor.\n");
         printf("Valor 1: ");
@@ -15,7 +15,7 @@ int main()
         scanf("%d", &b);
     }
     c = a % b;
-    while (c != 0);
+    while (c != 0)
     {
         a = b;
         b = c;
