9ac36157540009e7ff6ff97a4b7eaf2f27f808f6
katukis <katukis@outlook.com>
2025-11-07 20:22:32 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,13 +1,26 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-  int a, b, c
-  do
-  {    
+    int a, b, c;
     printf("Valor 1: ");
     scanf("%d", &a);
-    print("Valor 2: ");
+    printf("Valor 2: ");
     scanf("%d", &b);
-  }
-  while(a
-  
+    while (a <= 0 || b <= 0 || a < b);
+    {
+        printf("\nLos valores deben ser positivos y el primero mayor.\n");
+        printf("Valor 1: ");
+        scanf("%d", &a);
+        printf("Valor 2: ");
+        scanf("%d", &b);
+    }
+    resto = a % b;
+    while (c != 0);
+    {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    printf("\nEl MCD es: %d\n", b);
+    return 0;
+}
